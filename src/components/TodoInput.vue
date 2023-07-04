4db4b773064d4cae4b7f5ce480430d2e87ae5f60
JJHaEun <yj05037@gmail.com>
2023-07-04 12:43:12 +0900

    fontawesome 작동안되어 텍스트로 버튼에 적용

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,11 +1,69 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" />
+    <!-- v-model의 값을 저장. -->
+    <!-- <button >add</button> -->
+    <span class="appContainer" @click="addTodo">
+      <!-- <i class="fa fa-plus addBtn" aria-hidden="true"></i>222 -->
+      <span class="addBtn">+</span>
+    </span>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      // data 안의 newTodoItem에 접근
+      console.log(this.newTodoItem);
+      // 버튼 클릭할때마다
+
+      // 저장하는 로직후
+      localStorage.setItem("todoList", this.newTodoItem);
+      // 입력값 초기화
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.appContainer {
+  float: right;
+  height: 100%;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.addBtn {
+  font-size: 30px;
+  font-weight: 800;
+  color: white;
+  /* vertical-align: middle; */
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount("#app");
+
+// new Vue({
+//   el:"#app",
+//   render:h=>h(App)
+// })
