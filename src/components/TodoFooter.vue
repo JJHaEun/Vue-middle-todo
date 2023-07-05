ad10fb98c2e6d1fda74f0d4fef748a26c4a56e60
JJHaEun <yj05037@gmail.com>
2023-07-05 11:05:49 +0900

    localStorage.clear사용하여 로컬스토리지 비우기.(전체 삭제 구현)

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>footer</div>
+  <div class="clearAllContainer">
+    <!-- 모든 리스트 지우기 버튼! -->
+    <span class="clearAllBtn" @click="clearAll">
+      Clear All <i class="fa fa-eraser" aria-hidden="true"></i>
+    </span>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    clearAll() {
+      localStorage.clear(); // 로컬스토리지 지우기(전부 지우기) => 로컬스토리지의 금지표시(Clear All)와 동알
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: red;
+  display: block;
+}
+.fa-eraser {
+  color: #8763fb;
+}
+</style>
