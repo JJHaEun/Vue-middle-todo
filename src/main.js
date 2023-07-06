d3b67b42e0fab458d5a4c22e2e47d3cc0496a3f3
JJHaEun <yj05037@gmail.com>
2023-07-06 11:18:17 +0900

    store모든 컴포넌트에서 사용가능하도록 연결해주기 20230706

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { store } from "./store/store";
+// store가져오기
 
 new Vue({
   render: h => h(App),
+  // store넣기.
+  store,
 }).$mount("#app");
 
 // new Vue({
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// export const store =>  store 라는 변수 다른곳에서 사용가능. => 다른 컴포넌트에서 접근하려면 최상단 main.js에서 import 해서 연결해주기
 export const store = new Vuex.Store({
   // Vuex설치, 등록
 });
