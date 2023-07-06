5b2764f9f7bc6e195e813602a138f977e9897cd3
JJHaEun <yj05037@gmail.com>
2023-07-06 12:55:02 +0900

    state값 넣기

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,20 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.getItem("todoListItem") !== "[]") {
+      arr.push(JSON.parse(localStorage.getItem("todoListItem")));
+    }
+    return arr;
+  },
+};
+
 // export const store =>  store 라는 변수 다른곳에서 사용가능. => 다른 컴포넌트에서 접근하려면 최상단 main.js에서 import 해서 연결해주기
 export const store = new Vuex.Store({
   // Vuex설치, 등록
+  state: {
+    todoItems: storage.fetch(),
+  },
 });
