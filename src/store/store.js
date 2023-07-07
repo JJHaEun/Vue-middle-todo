c1bc6d7b149282e3e4bf3a505997e2eaa975bf99
JJHaEun <yj05037@gmail.com>
2023-07-07 10:40:51 +0900

    store모듈화하기-2

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as getters from "./getters";
-import * as mutations from "./mutations";
+// import * as getters from "./getters";
+// import * as mutations from "./mutations";
+import todoApp from "./modules/todoApp";
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    let arr;
-    if (localStorage.getItem("todoListItem")) {
-      console.log(JSON.parse(localStorage.getItem("todoListItem")));
-      arr = JSON.parse(localStorage.getItem("todoListItem"));
-    }
-    console.log(arr);
+// const storage = {
+//   fetch() {
+//     let arr;
+//     if (localStorage.getItem("todoListItem")) {
+//       console.log(JSON.parse(localStorage.getItem("todoListItem")));
+//       arr = JSON.parse(localStorage.getItem("todoListItem"));
+//     }
+//     console.log(arr);
 
-    return arr;
-  },
-};
+//     return arr;
+//   },
+// };
 
 // export const store =>  store 라는 변수 다른곳에서 사용가능. => 다른 컴포넌트에서 접근하려면 최상단 main.js에서 import 해서 연결해주기
 export const store = new Vuex.Store({
   // Vuex설치, 등록
-  state: {
-    todoItems: storage.fetch(),
+  // state: {
+  // todoItems: storage.fetch(),
+  // },
+  // getters,
+  // mutations,
+  modules: {
+    todoApp,
   },
-  getters,
-  mutations,
 });
